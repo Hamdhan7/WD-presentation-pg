d87225e0f0df2cf9ae78bea0d038f0b2afe29290
Hamdhan7 <116866985+Hamdhan7@users.noreply.github.com>
2023-03-29 13:26:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,3 @@
+// setTimeout(function() {
+//     window.location = "https://example.com"; // replace with your desired URL
+//   }, 4000); // time is in milliseconds (4 seconds = 4000 milliseconds)
